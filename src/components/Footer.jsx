9910a3ef09ca7b726b6d87e085e99f81938baab8
basil-hawkins <isomura.yudai@gmail.com>
2021-10-18 11:54:35 +0900

    フッターの名前変更

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
 export const Footer = () => {
-	return (
-		<footer className="footer">
-			<p>Copyright &copy; 2021 miyasan {"&"} yasui</p>
-		</footer>
-	);
+  return (
+    <footer className="footer">
+      <p>Copyright &copy; 2021 ゆうだい {"&"} yasui</p>
+    </footer>
+  );
 };
